88a92bf676e9d2074d59f8faa3265a52c89c2317
kamilycarraro <kamily.pezzini@escola.pr.gov.br>
2025-10-10 08:02:14 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,15 @@
+criaCartao(
+'Programação',
+'O que é Python?',
+'O python é uma linguagem de programação'
+)
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da frança é Paris'
+)
+criaCartao(
+    'Programação',
+    'O que é uma funçaõ?',
+    'Uma função é um bloco de códigos que executa alguma tarefa'
+)
