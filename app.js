0fed54fbb07f624418718a709dd2d193a8a91f7e
kamilycarraro <kamily.pezzini@escola.pr.gov.br>
2025-10-22 08:50:37 -0300

    .

---
+++ app.js
@@ -0,0 +1,5 @@
+function criaCartao(categoria,pergunta,resposta){
+    let container=document.getElementByld('container')
+    let cartao = document.createElement('article')
+    CredentialsContainer.className='cartao'
+}
